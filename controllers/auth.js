5435cf266e815e8cb6c97682bdad6d285d7cee8b
Fedor Romanovschi <110813692+lordferro@users.noreply.github.com>
2023-07-25 10:38:55 +0200

    add token after registration

--- controllers/auth.js
+++ controllers/auth.js
@@ -26,9 +26,18 @@ const register = async (req, res) => {
     avatarURL,
   });
 
+  const payload = {
+    id: newUser._id,
+  };
+
+  const token = jwt.sign(payload, SECRET_KEY, { expiresIn: "23h" });
+
+  await User.findByIdAndUpdate(newUser._id, { token });
+
   res.status(201).json({
     email: newUser.email,
     id: newUser._id,
+    token,
   });
 };
 
